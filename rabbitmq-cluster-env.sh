4ce2e9b5ff2428c084543afab76a3c783b7f0ba4
root <root@ip-10-179-185-243.ec2.internal>
2017-04-07 08:59:03 +0000

    Evironment Property Change

--- rabbitmq-cluster-env.sh
+++ rabbitmq-cluster-env.sh
@@ -7,17 +7,20 @@
 # Maintainer Vyom Tandon <vyom.x.tandon.-nd@disney.com>
 #====================================================================
 
+# CleanUp previous files.
+rm -rf /etc/sysconfig/rabbitmq-join
+
 /usr/bin/sh -c "/usr/bin/echo -n RABBITMQ_JOIN= > /etc/sysconfig/rabbitmq-join"
 
 /usr/bin/sh -c "/usr/local/bin/aws ec2 describe-instances --filters 'Name=tag:Name,Values=gam-ats-rmq-latest*' --output text --query 'Reservations[*].Instances[*].PrivateDnsName' --region=us-east-1 | tr '\n' ';' >> /etc/sysconfig/rabbitmq-join"
 
 export $(head -n 1 /etc/sysconfig/rabbitmq-join)
 
-echo ******************************************************************************************************************************************
-echo ${RABBITMQ_JOIN%;*}
-echo ******************************************************************************************************************************************
+printf ******************************************************************************************************************************************
+printf ${RABBITMQ_JOIN%;*}
+printf ******************************************************************************************************************************************
 
 # get script path
 export SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-/usr/bin/sh $SCRIPT_PATH/rabbitmq_install.sh -i -c ZTXOCZYZWBCFLBPOBEUQ -j ${RABBITMQ_JOIN%;*}
+/usr/bin/sh $SCRIPT_PATH/rabbitmq_install.sh -c ZTXOCZYZWBCFLBPOBEUQ -ij ${RABBITMQ_JOIN%;*}
